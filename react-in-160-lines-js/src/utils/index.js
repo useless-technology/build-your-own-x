0224ac4011a0cb095aaee4493faa36511daaf3c5
ryanmoyo <iherewithmyheart@gmail.com>
2021-08-17 17:15:44 +0800

    🎨 export *

---
+++ react-in-160-lines-js/src/utils/index.js
@@ -0,0 +1,6 @@
+import render from './render';
+import setAttribute from './setAttribute';
+import patch from './patch';
+import Component from './component.class';
+export { render, setAttribute, patch, Component };
+export default { render, setAttribute, patch, Component };
